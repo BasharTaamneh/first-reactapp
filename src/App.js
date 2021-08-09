743d69833dc927077158cd44f1343ff09b74686d
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-08-09 22:48:19 +0300

    done React State and Props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import react from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main'
+
 class App extends react.Component {
   render() {
     return (

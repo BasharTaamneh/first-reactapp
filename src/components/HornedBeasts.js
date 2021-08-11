f677945d61f3d9c1f0b8ffb3d8d1d8c1c1854d67
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-08-11 20:10:45 +0300

    edit passing functions as props and don with it

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -2,10 +2,9 @@ import react from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal'
-import HornedData from './HornedData.json'
 
 class HornedBeasts extends react.Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -19,14 +18,13 @@ class HornedBeasts extends react.Component {
             NumOfVot: this.state.NumOfVot + 1
         })
     }
+
     render() {
-        // console.log(HornedData.find(this.props.keyModal ))
-        // console.log(this.props.keyModal)
         return (
             <div className='container'>
 
                 <Card style={{ width: '19rem' }}>
-                    <Card.Img  onClick={this.props.Modalview}  src={this.props.src} />
+                    <Card.Img onClick={()=>this.props.show(this.props.data)} src={this.props.src} />
                     <Card.Body>
                         <Card.Title>{this.props.imgTitle}</Card.Title>
                         <Card.Text>
@@ -35,19 +33,6 @@ class HornedBeasts extends react.Component {
                         <Button onClick={this.IncrNumOfVot} variant="primary">Number of votes: {this.state.NumOfVot}</Button>
                     </Card.Body>
                 </Card>
-
-                <Modal show={this.props.showstatuse} >
-                    <Modal.Header closeButton onHide={this.props.Modalview}>
-                        <Modal.Title>{this.props.imgTitle}</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body><img src={this.props.src}alt={this.props.imgTitle} /> <p>{this.props.description}</p></Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={this.props.Modalview}>
-                            Close
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-
             </div>
         )
     }
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,45 +2,27 @@ import react from 'react';
 import HornedBeasts from './HornedBeasts';
 import HornedData from './HornedData.json'
 import './Main.css';
+
 class App2 extends react.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            show: false
-        }
-    }
-
-    handleModal = () => {
-        this.setState({
-            show: !this.state.show
-        })
-    }
-
-
     render() {
-        console.log()
         return (
             <>
                 {
                     HornedData.map((items, idx) => {
-                        // console.log(idx)
                         return (
                             <HornedBeasts
                                 keyModal={idx}
                                 imgTitle={items.title}
                                 src={items.image_url}
                                 description={items.description}
-                                Modalview={this.handleModal}
-                                showstatuse={this.state.show}
-
+                                data={items}
+                                show={this.props.show}
                             />
-
                         );
                     })
 
                 }
-                {/* {HornedData.find(element => element.keyModal)} */}
             </>
         )
     }
